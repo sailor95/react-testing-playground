bce98274f563925d0a0cbdee62bffd7268692f8f
David Huang <david.huang@getac.com.tw>
2019-11-28 17:32:42 +0800

    Create test for CommentBox

---
+++ src/components/__tests__/CommentBox.test.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import { mount } from 'enzyme';
+import CommentBox from 'components/CommentBox';
+
+let wrapped;
+
+beforeEach(() => {
+  wrapped = mount(<CommentBox />);
+});
+
+afterEach(() => {
+  wrapped.unmount();
+});
+
+it('has a text area and a button', () => {
+  expect(wrapped.find('textarea').length).toEqual(1);
+  expect(wrapped.find('button').length).toEqual(1);
+});
+
+// Create a test code block
+describe('the text area', () => {
+  beforeEach(() => {
+    // Mock a 'onChange' result in textarea
+    wrapped.find('textarea').simulate('change', {
+      target: { value: 'new comment' }
+    });
+    // NOTE: Since React component update is async, we need to force an update
+    wrapped.update();
+  });
+
+  it('has a text area that users can type in', () => {
+    expect(wrapped.find('textarea').prop('value')).toEqual('new comment');
+  });
+
+  it('when form is submitted, text area gets emptied', () => {
+    wrapped.find('form').simulate('submit');
+    wrapped.update();
+
+    expect(wrapped.find('textarea').prop('value')).toEqual('');
+  });
+});
+
