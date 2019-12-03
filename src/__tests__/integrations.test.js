d59b52fe718acd6c4436b4c5f34a410173f6da06
David Huang <david.huang@getac.com.tw>
2019-12-03 10:51:17 +0800

    Create integration test for api fetch process

---
+++ src/__tests__/integrations.test.js
@@ -0,0 +1,52 @@
+import React from 'react';
+import { mount } from 'enzyme';
+import moxios from 'moxios';
+
+import Root from 'Root';
+import App from 'components/App';
+
+beforeEach(() => {
+  moxios.install();
+  moxios.stubRequest('http://jsonplaceholder.typicode.com/comments', {
+    status: 200,
+    response: [{ name: 'Fetched #1' }, { name: 'Fetched #2' }]
+  });
+});
+
+afterEach(() => {
+  moxios.uninstall();
+});
+
+it('can fetch a list of comments and display them', (done) => {
+  // Attempt to render the entire app
+  const wrapped = mount(
+    <Root>
+      <App />
+    </Root>
+  );
+
+  // find the 'fetchComments' button and click it
+  wrapped.find('.fetch-comments').simulate('click');
+
+  // expect to find a list of comments
+
+  moxios.wait(() => {
+    wrapped.update();
+
+    expect(wrapped.find('li')).toHaveLength(2);
+
+    done();
+    wrapped.unmount();
+  });
+
+
+
+  // setTimeout(() => {
+  //   wrapped.update();
+
+  //   expect(wrapped.find('li')).toHaveLength(2);
+
+  //   done();
+  //   wrapped.unmount();
+  // }, 100);
+});
--- src/components/CommentBox.js
+++ src/components/CommentBox.js
@@ -33,7 +33,9 @@ class CommentBox extends Component {
             <button>Submit Comment</button>
           </div>
         </form>
-        <button onClick={this.props.fetchComments}>Fetch Comment</button>
+        <button className="fetch-comments" onClick={this.props.fetchComments}>
+          Fetch Comment
+        </button>
       </div>
     );
   }
